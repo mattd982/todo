d612bce14dbf9dd7d0f3e52ad5d8951b73e9483d
matt day <mattdxc982@gmail.com>
2019-12-31 02:13:30 +0000

    lesson 20

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,18 +1,20 @@
  $(function() {
-    // The taskHtml method takes in a JavaScript representation
-    // of the task and produces an HTML representation using
-    // <li> tags
-    function taskHtml(task) {
-      var checkedStatus = task.done ? "checked" : "";
-      var liElement = '<li><div class="view"><input class="toggle" type="checkbox"' +
-        " data-id='" + task.id + "'" +
-        checkedStatus +
-        '><label>' +
-         task.title +
-         '</label></div></li>';
+  // The taskHtml method takes in a JavaScript representation
+  // of the task and produces an HTML representation using
+  // <li> tags
+  function taskHtml(task) {
+    var checkedStatus = task.done ? "checked" : "";
+    var liClass = task.done ? "completed" : "";
+    var liElement = '<li id="listItem-' + task.id +'" class="' + liClass + '">' +
+    '<div class="view"><input class="toggle" type="checkbox"' +
+      " data-id='" + task.id + "'" +
+      checkedStatus +
+      '><label>' +
+       task.title +
+       '</label></div></li>';
 
-      return liElement;
-    }
+    return liElement;
+  }
 
     // toggleTask takes in an HTML representation of the
     // an event that fires from an HTML representation of
@@ -28,7 +30,12 @@
         task: {
           done: doneValue
         }
-      });
+      }).success(function(data) {
+        var liHtml = taskHtml(data);
+        var $li = $("#listItem-" + data.id);
+        $li.replaceWith(liHtml);
+        $('.toggle').change(toggleTask);
+      } );
     }
 
     $.get("/tasks").success( function( data ) {
